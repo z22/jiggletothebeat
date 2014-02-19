786cd2dced91dd29cac275691c73b9e81dd0efab
Mel Zhang <zeemelon@gmail.com>
2014-02-18 23:46:10 -0800

    initialized git

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Jiggletothebeat::Application.config.secret_key_base = '04ab687e04a05ba1518cd286ef85f36dc756e80962a15673db21ad7a2aadc60ea7e42d063fa49b08f598c112bcf68843f714a522e8c5de7d63e2c47a4c6f7fff'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Jiggletothebeat::Application.config.session_store :cookie_store, key: '_jiggletothebeat_session'
