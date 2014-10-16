c8dfe271e1ba49481f3656529497a8c454dd3a27
Mel Zhang <zeemelon@gmail.com>
2014-10-16 00:37:53 -0700

    precompiled assets

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -34,10 +34,10 @@ Jiggletothebeat::Application.configure do
   config.action_mailer.delivery_method = :smtp
 
   config.action_mailer.smtp_settings = {
-    address: “smtp.gmail.com”,
+    address: "smtp.gmail.com",
     port: 587,
     domain: ENV["GMAIL_DOMAIN"],
-    authentication: “plain”,
+    authentication: "plain",
     enable_starttls_auto: true,
     user_name: ENV["GMAIL_USERNAME"],
     password: ENV["GMAIL_PASSWORD"]
